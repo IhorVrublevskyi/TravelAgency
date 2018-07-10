4af9b0735c311b7d9e293c6aac31175ba6accb4b
marisha92 <novitska.mv@gmail.com>
2018-07-10 16:01:51 +0300

    ViewUrls are updated

--- src/main/java/org/lv326java/two/travelagency/controllers/ViewUrls.java
+++ src/main/java/org/lv326java/two/travelagency/controllers/ViewUrls.java
@@ -10,7 +10,9 @@ public enum ViewUrls {
     HOTELS_JSP("/WEB-INF/pages/hotels.jsp"),
     THANKS_JSP("/WEB-INF/pages/thanks.jsp"),
     VISA_JSP("/WEB-INF/pages/visa.jsp"),
-    SEARCH_JSP("/WEB-INF/pages/search.jsp");
+    SEARCH_JSP("/WEB-INF/pages/search.jsp"),
+
+    ADMIN_COUNTRY_JSP("/WEB-INF/pages/admin/countries.jsp");
     //
     private String url;
 
