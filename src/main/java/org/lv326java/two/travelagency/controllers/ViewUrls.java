8744c7cabe8d487fc8f5f384c9aebabd4d6efdc9
Igor Vrublevsky <ihor.jv@gmail.com>
2018-07-10 17:23:47 +0300

    Resolved conflict

--- src/main/java/org/lv326java/two/travelagency/controllers/ViewUrls.java
+++ src/main/java/org/lv326java/two/travelagency/controllers/ViewUrls.java
@@ -5,14 +5,15 @@ public enum ViewUrls {
     INFO_ABOUT_SELECTED_HOTEL_JSP("/WEB-INF/pages/infoAboutSelectedHotel.jsp"),
     HOTEL_CONFIRM_JSP("/WEB-INF/pages/hotelConfirm.jsp"),
     USER_CABINET_JSP("/WEB-INF/pages/userCabinet.jsp"),
-    ADMIN_CABINET_JSP("/WEB-INF/pages/admin/adminsCabinet.jsp"),
+    ADMIN_CABINET_JSP("/WEB-INF/pages/adminsCabinet.jsp"),
     REGISTRATION_JSP("/WEB-INF/pages/registration.jsp"),
     HOTELS_JSP("/WEB-INF/pages/hotels.jsp"),
     THANKS_JSP("/WEB-INF/pages/thanks.jsp"),
     VISA_JSP("/WEB-INF/pages/visa.jsp"),
     SEARCH_JSP("/WEB-INF/pages/search.jsp"),
-
-    ADMIN_COUNTRY_JSP("/WEB-INF/pages/admin/countries.jsp");
+    ADMIN_CABINET_JSP("/WEB-INF/pages/adminsCabinet.jsp"),
+    ADMIN_COUNTRY_JSP("/WEB-INF/pages/admin/countries.jsp"),
+    ADMIN_CITIES_JSP("/WEB-INF/pages/admin/cities.jsp");
     //
     private String url;
 
